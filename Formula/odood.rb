41f89ccc5f3232acff3d7c57e100c412ff59e10d
Dmytro Katyukha <dmytro.katyukha@gmail.com>
2025-12-24 09:48:05 +0200

    Bump Odood version to 0.5.2

--- Formula/odood.rb
+++ Formula/odood.rb
@@ -1,16 +1,9 @@
 class Odood < Formula
   desc "Manage local development odoo installations with ease"
   homepage "https://katyukha.github.io/Odood/"
-  url "https://github.com/katyukha/Odood/archive/refs/tags/v0.5.1.tar.gz"
-  sha256 "858e072a56ac0af44ae4f7ea25a3212a0e6896f9149968c850e714aef9868c9d"
+  url "https://github.com/katyukha/Odood/archive/refs/tags/v0.5.2.tar.gz"
+  sha256 "a957fb598e51b0161506f0cf51c2443592aed1caf59efa00dd90e33e2957b333"
   license "MPL-2.0"
-
-  bottle do
-    root_url "https://github.com/katyukha/homebrew-odood/releases/download/odood-0.5.1"
-    sha256                               arm64_sequoia: "9af3f7648a148c36c79f69636d468c411fcda1f956b4da9c2be362800bc11df2"
-    sha256                               arm64_sonoma:  "a822f186d65fc2334d96b2815ff8fa7bbdc20d7efd3e08d3ce0377bc04ed66b3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3e96b81d907edda35adfd044d0f1e016a558351809492dc1cf4b4ec7ee3a5cbf"
-  end
 
   depends_on "dub" => :build
   depends_on "ldc" => :build
